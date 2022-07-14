ef4a9c397b0b5f92f59fef34af6e4237c6466204
dan3aqm <88002032+dan3aqm@users.noreply.github.com>
2022-07-14 13:50:05 -0700

    Create f-functions-7141.js

---
+++ q702-lessons/q714-functions/f-functions-7141.js
@@ -0,0 +1,15 @@
+
+///function expressions ex 
+
+//define a function with function-expresion syntax 
+let varContainingFunction = function() {
+    let varInFunction = "I'm in a function.";
+    console.log("hi there!", varInFunction);
+};
+
+//call/invoke the fn
+varContainingFunction();
+
+
+///
+
