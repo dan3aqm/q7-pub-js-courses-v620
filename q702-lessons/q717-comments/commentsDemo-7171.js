9cdd6cf8b131986a3f0ae4b3dbc98338eab1f98f
dan3aqm <88002032+dan3aqm@users.noreply.github.com>
2022-07-17 11:56:12 -0700

    Create commentsDemo-7171.js

---
+++ q702-lessons/q717-comments/commentsDemo-7171.js
@@ -0,0 +1,39 @@
+
+///-- single line comments: 
+
+// Prints 5 to the console
+console.log(5);
+console.log(5);  // Prints 5 
+
+
+///-- multiline comments: 
+
+/*
+This is all commented 
+console.log(10);
+None of this is going to run!
+console.log(99);
+*/
+
+
+///-- inline comments: 
+
+console.log(/*IGNORED!*/ 5);  // Still just prints 5 
+
+
+
+///-- ex. 2 
+
+//Opening Line
+console.log('It was love at first sight.');
+
+/*
+console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
+console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
+console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
+console.log('If it became jaundice they could treat it.');
+console.log('If it didn\'t become jaundice and went away they could discharge him.');
+console.log('But this just being short of jaundice all the time confused them.');
+*/
+
+
