317161d61ffbae6c68a18798c76afdffb0e0ea5e
dan3aqm <88002032+dan3aqm@users.noreply.github.com>
2022-07-17 15:25:35 -0700

    Create npm-commands-7171.sh

---
+++ q702-lessons/q7-subcourse-717-npm/npm-commands-7171.sh
@@ -0,0 +1,47 @@
+
+### version subcommand
+PS C:\> npm --version
+
+8.12.1
+
+
+### init subcommand
+PS C:\> npm init 
+
+This utility will walk you through creating a package.json file.
+It only covers the most common items, and tries to guess sensible defaults.
+
+See `npm help init` for definitive documentation on these fields
+and exactly what they do.
+
+Use `npm install <pkg>` afterwards to install a package and
+save it as a dependency in the package.json file.
+
+Press ^C at any time to quit.
+package name: (q7-npmdemo-pj)
+version: (1.0.0)
+description: An NPM Demo Project
+entry point: (index.js)
+test command:
+git repository:
+keywords:
+author:
+license: (ISC)
+About to write to C:\q7-npmDemo-pj\package.json:
+
+{
+  "name": "q7-npmdemo-pj",
+  "version": "1.0.0",
+  "description": "An NPM Demo Project",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC"
+}
+
+
+Is this OK? (yes) yes
+PS C:\\q7-npmDemo-pj> 
+
